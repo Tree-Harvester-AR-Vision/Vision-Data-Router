eb959595327129b08698c6253a2252314fc6010a
Endersky78 <jackhart591@gmail.com>
2023-01-16 12:59:50 -0800

    Finished routing for server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const WebSocket = require('ws')
+const events = require('events')
 
 var port = process.env.PORT || 8000
+
+var eventEmitter = new events.EventEmitter()
+
+var BoundingBoxes
 
 const wss = new WebSocket.Server({ port: port }, () => {
     console.log(`===> Server Started on port ${port}`)
@@ -9,7 +14,29 @@ const wss = new WebSocket.Server({ port: port }, () => {
 wss.on('connection', function connection(ws) {
     console.log("connection!")
     ws.on('message', (data) => {
-        console.log(`Recieved ${data} from client`)
+        if (data == 'T') {
+            while(true) {
+                let breakSig = false
+                ws.on('message', (boxData) => {
+                    if (boxData == 'Connection End') {
+                        breakSig = true
+                    } else {
+                        BoundingBoxes = boxData
+                        eventEmitter.emit('New Box Data')
+                    }
+                })
+
+                if (breakSig) break
+            }
+        } else if (data == 'R') {
+            while(true) {
+                let breakSig = false
+                let eventHandler = function() {
+                    ws.send(BoundingBoxes)
+                }
+                eventEmitter.on('New Box Data', eventHandler)
+            }
+        }
     })
 })
 
